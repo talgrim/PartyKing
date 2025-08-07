d8efb0c7e81483b65f00c018cb25a4e1dd63b040
talgr <tsa@232>
2025-08-07 15:14:50 +0200

    Deleted image data from DB

---
+++ PartyKing.Domain/Entities/SlideshowImage.cs
@@ -0,0 +1,29 @@
+﻿using PartyKing.Domain.Models.Slideshow;
+
+namespace PartyKing.Domain.Entities;
+
+public class SlideshowImage
+{
+    private SlideshowImage()
+    {
+    }
+
+    public SlideshowImage(SlideshowImageWriteModel writeModel)
+    {
+        ImageUrl = writeModel.ImageUrl;
+        ContentType = writeModel.ContentType;
+    }
+
+    public Guid Id { get; set; } = Guid.CreateVersion7();
+    public string ImageUrl { get; set; }
+    public string ContentType { get; set; }
+
+    public SlideshowImageReadModel ToReadModel()
+    {
+        return new SlideshowImageReadModel
+        {
+            ImageUrl = ImageUrl,
+            ContentType = ContentType
+        };
+    }
+}
