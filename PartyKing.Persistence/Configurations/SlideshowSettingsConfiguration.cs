22cbd804e8517f66775a9366b552749683fee07d
talgr <tsa@232>
2025-08-15 00:22:55 +0200

    Moved Slideshow settings to DB

---
+++ PartyKing.Persistence/Configurations/SlideshowSettingsConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PartyKing.Domain.Entities;
+
+namespace PartyKing.Persistence.Configurations;
+
+public class SlideshowSettingsConfiguration : IEntityTypeConfiguration<SlideshowSettings>
+{
+    public void Configure(EntityTypeBuilder<SlideshowSettings> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id)
+            .ValueGeneratedNever();
+
+        builder.HasData(new SlideshowSettings
+        {
+            AutoRepeat = false,
+            StartFromBeginning = false,
+            SlideTime = TimeSpan.FromSeconds(20)
+        });
+    }
+}
