c47efa8b30d138a8b8d7736ae565c893616b88d3
talgr <tsa@232>
2025-08-14 23:18:08 +0200

    LED screen adjustments

--- PartyKing.UI/src/App.tsx
+++ PartyKing.UI/src/App.tsx
@@ -1,19 +1,34 @@
 import {Layout} from './components/Layout';
 import {ComposeComponents} from "@/utils/ComposeComponents";
 import {Outlet} from "react-router-dom";
+import {createTheme, CssBaseline, ThemeProvider} from "@mui/material";
 
 // import {Provider} from "@/contexts/auth/provider";
 
+const themeDark = createTheme({
+  palette: {
+    background: {
+      default: "#000000"
+    },
+    text: {
+      primary: "#ffffff",
+    }
+  }
+})
+
 export function App() {
   return (
-    <ComposeComponents
-      components={[
-        // Provider
-      ]}
-    >
-      <Layout>
-        <Outlet/>
-      </Layout>
-    </ComposeComponents>
+    <ThemeProvider theme={themeDark}>
+      <CssBaseline />
+      <ComposeComponents
+        components={[
+          // Provider
+        ]}
+      >
+        <Layout>
+          <Outlet/>
+        </Layout>
+      </ComposeComponents>
+    </ThemeProvider>
   );
 }
